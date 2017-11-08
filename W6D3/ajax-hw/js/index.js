84ae2fca8bcc3b6cb75c4c4489ac3fde62222662
Edan Lewis <edan@lewisarts.com>
2017-11-07 20:24:30 -0800

    Adds basic AJAX request functionality.

---
+++ W6D3/ajax-hw/js/index.js
@@ -0,0 +1,17 @@
+console.log("Hello from the JavaScript console!");
+
+// Your AJAX request here
+
+$.ajax({
+  type: 'GET',
+  url: "http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b",
+  success: function(data) {
+    console.log(data);
+  },
+  error() {
+    console.log("whoops, this is embarassing");
+  }
+});
+
+// Add another console log here, outside your AJAX request
+console.log("BLASTOFF!");
